bb672c145cdd08106351fece1b73a2a7d4668c55
Alexei Znamensky <russoz@cpan.org>
2011-06-29 19:54:07 -0300

    using Moose "builder" rather than "default" (almost everywhere)
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/DataFlow/Proc/JSON.pm
+++ lib/DataFlow/Proc/JSON.pm
@@ -13,11 +13,9 @@ extends 'DataFlow::Proc::Converter';
 use namespace::autoclean;
 use JSON::Any;
 
-has '+policy' => (
-    'default' => sub {
-        return shift->direction eq 'CONVERT_TO' ? 'ArrayRef' : 'Scalar';
-    },
-);
+sub _policy {
+    return shift->direction eq 'CONVERT_TO' ? 'ArrayRef' : 'Scalar';
+}
 
 has '+converter' => (
     lazy    => 1,
@@ -37,21 +35,17 @@ has '+converter' => (
 
 has '+converter_opts' => ( 'init_arg' => 'json_opts', );
 
-has '+converter_subs' => (
-    'lazy'    => 1,
-    'default' => sub {
-        my $self = shift;
-        return {
-            'CONVERT_TO' => sub {
-                return $self->converter->to_json($_);
-            },
-            'FROM_JSON' => sub {
-                return $self->converter->from_json($_);
-            },
-        };
-    },
-    'init_arg' => undef,
-);
+sub _build_subs {
+    my $self = shift;
+    return {
+        'CONVERT_TO' => sub {
+            return $self->converter->to_json($_);
+        },
+        'FROM_JSON' => sub {
+            return $self->converter->from_json($_);
+        },
+    };
+}
 
 __PACKAGE__->meta->make_immutable;
 
